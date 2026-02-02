6b28a345a235b19be0749537d58ff8a95d0954f6
harishhans29@gmail.com <harishhans29@gmail.com>
2026-02-02 23:14:13 +0530

    Initial Commit

---
+++ accounts/src/main/java/com/hans/accounts/entity/Accounts.java
@@ -0,0 +1,26 @@
+package com.hans.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Accounts extends BaseEntity{
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
